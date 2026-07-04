4ec59dd408852dd7ffc7f1cda505e2c6477b3948
Omi005 <khedekaromkar737@gmail.com>
2026-07-04 07:14:53 +0530

    Fix media configuration and display product images

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'usermedia')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/products/'
 LOGOUT_REDIRECT_URL = '/products/'
